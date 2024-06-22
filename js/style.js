ab89fb218935b1aa19504695b06875fbb195d6ae
Madhavi Neupane <86922967+madhavi-neupane@users.noreply.github.com>
2024-06-22 15:26:06 +0545

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,38 @@
+// nav
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+function toggleReadMore(button) {
+  var dotId = button.getAttribute('data-dot');
+  var moreId = button.getAttribute('data-more');
+  var dots = document.getElementById(dotId);
+  var moreText = document.getElementById(moreId);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    button.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    button.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// function navbar() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
